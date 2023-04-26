547f641ef0f8a8bc47c2772e884b2de2a28629ac
hayden365 <ysmh100@gmail.com>
2023-04-27 04:20:39 +0900

    Module 1

---
+++ 1. Blockchain Intuition/blockchain.py
@@ -0,0 +1,16 @@
+# Module 1 - Create a Blockchain
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+    
